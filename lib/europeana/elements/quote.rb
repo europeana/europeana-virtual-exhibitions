c31d9e0e88315043c90fd5ffcdd18121851b59fa
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-03-07 13:49:12 +0100

    Improve quote

--- lib/europeana/elements/quote.rb
+++ lib/europeana/elements/quote.rb
@@ -1,6 +1,14 @@
 module Europeana
   module Elements
     class Quote < Europeana::Elements::Base
+      protected
+      def data
+        {
+          quote: @element.content_by_name("quote").essence.body,
+          stripped_quote: @element.content_by_name("quote").essence.stripped_body,
+          quotee: @element.content_by_name("quotee").essence.body
+        }
+      end
     end
   end
 end
