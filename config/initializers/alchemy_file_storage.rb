b03deceda56744d75209955279c22c52d6c9122c
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-03-09 13:25:53 +0100

    Add config for swift storage

---
+++ config/initializers/alchemy_file_storage.rb
@@ -0,0 +1,23 @@
+require 'dragonfly'
+require 'dragonfly/swift_data_store'
+
+openstack_defaults = {
+  openstack_auth_url: ENV.fetch('OPENSTACK_AUTH_URL', ''),
+  openstack_username: ENV.fetch('OPENSTACK_USERNAME', ''),
+  openstack_api_key: ENV.fetch('OPENSTACK_API_KEY', ''),
+  storage_headers: {'x-amz-acl' => 'public-read'},
+  url_scheme: 'https'
+}
+Dragonfly.app(:alchemy_pictures).configure do
+  plugin :imagemagick
+  verify_urls false
+  datastore :swift,
+    { container_name: ENV.fetch('IMAGES_CONTAINER_NAME') }.merge(openstack_defaults)
+  url_host ENV.fetch('CDN_HOST', 'http://localhost:3000')
+end
+
+Dragonfly.app(:alchemy_attachments).configure do
+  datastore :swift,
+    { container_name: ENV.fetch('ATTACHMENTS_CONTAINER_NAME') }.merge(openstack_defaults)
+  url_host ENV.fetch('CDN_HOST', 'http://localhost:3000')
+end
