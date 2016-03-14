44d26601ba699d39b34af79dfc532f6c90535568
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-03-14 12:33:41 +0100

    Protocol agnostic

--- lib/europeana/gsub.rb
+++ lib/europeana/gsub.rb
@@ -3,7 +3,7 @@ require 'rack-plastic'
 module Europeana
   class Gsub < Rack::Plastic
     def change_html_string(html)
-      html.gsub!("/pictures/", "#{ENV.fetch('CDN_HOST')}/pictures/")
+      html.gsub!("/pictures/", "//#{ENV.fetch('CDN_HOST')}/pictures/")
       html
     end
   end
