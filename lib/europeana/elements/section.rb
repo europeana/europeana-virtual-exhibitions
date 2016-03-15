5885267dc01b2d499e89744908f40c7631544a05
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-03-15 15:42:23 +0100

    Returing section_title as well

--- lib/europeana/elements/section.rb
+++ lib/europeana/elements/section.rb
@@ -1,6 +1,12 @@
 module Europeana
   module Elements
     class Section < Europeana::Elements::Base
+
+      def data
+        {
+          section_title: @element.content_by_name('title').essence.body
+        }
+      end
     end
   end
 end
