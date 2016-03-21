3de2ae25f71004014475c842b3ed943056f8c022
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-03-21 16:19:06 +0100

    Fix bug

--- lib/europeana/elements/intro.rb
+++ lib/europeana/elements/intro.rb
@@ -21,7 +21,7 @@ module Europeana
           title: @element.content_by_name("title").essence.body,
           sub_title: @element.content_by_name("sub_title").essence.body,
           image_credit: image_credit,
-          label:  @element.content_by_name("label").essence.body,
+          label:  @element.content_by_name("label").present? ? @element.content_by_name("label").essence.body : false,
           partner_image: versions("partner_logo")
         }
       end
