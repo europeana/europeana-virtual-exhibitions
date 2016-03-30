d50b9a3d5bab91b611be2b37aa114bcf9cd260e0
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-03-30 14:48:53 +0200

    Overriding domain settings

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,1 +1,2 @@
-Alchemy::Engine.routes.default_url_options[:host] = 'http://test.npc.eanadev.org'
+Alchemy::Engine.routes.default_url_options[:host] = ENV.fetch('APP_HOST', 'test.npc.eanadev.org')
+Alchemy::Engine.routes.default_url_options[:port] = ENV.fetch('APP_PORT', nil)
