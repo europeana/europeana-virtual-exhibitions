53d1999c2f0047de3a6261d18c3fd27183831595
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-04-06 16:38:06 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   get '/portal/exhibitions/sitemap-index.xml' => 'sitemap#index'
-  get '/portal/exhibitions/:locale/sitemap.xml' => 'sitemap#show', constraints: { :locale => /[a-z]{2}/}, as: :sitemap, defaults: {format: :xml}
+  get '/portal/:locale/exhibitions/sitemap.xml' => 'sitemap#show', constraints: { :locale => /[a-z]{2}/}, as: :sitemap, defaults: {format: :xml}
   get '/portal/exhibitions/robots.txt' => 'sitemap#robots'
   get '/portal/:locale/exhibitions/*urlname(.:format)' => 'alchemy/pages#show', as: :show_page, constraints: { :locale => /[a-z]{2}/}
 
