330b99384251b2ee9bdad3ecf0ef7ae2fa37f015
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-04-11 14:07:40 +0200

    Redo the way we generate different versions for the images

--- app/jobs/europeana/picture_versions_job.rb
+++ app/jobs/europeana/picture_versions_job.rb
@@ -4,12 +4,22 @@ class Europeana::PictureVersionsJob < ActiveJob::Base
   def perform(id)
     Rails.logger.info "Create versions for picture with id #{id}"
 
-    Europeana::Elements::Image::VERSIONS.values.compact.each do |size|
-      Rails.logger.info "Creating #{size} of picture #{id}"
+    Europeana::Elements::Image::VERSIONS.values.compact.each do |settings|
+      Rails.logger.info "Creating #{JSON.generate(settings)} of picture #{id}"
 
       picture = Alchemy::Picture.find(id)
 
-      Alchemy::PictureVersion.from_cache(picture.resize(size)).data
+      options = []
+
+      if settings[:format] && settings[:format] == 'jpeg'  && settings[:quality]
+        options << "-quality #{settings[:quality]}"
+      end
+
+      if !settings[:format]
+        settings[:format] = "jpeg"
+      end
+
+      Alchemy::PictureVersion.from_cache(picture.resize(settings[:size]).encode(settings[:format], options.join(" "))).data
       true
     end
   end
--- lib/europeana/mixins/image_version.rb
+++ lib/europeana/mixins/image_version.rb
@@ -2,23 +2,24 @@ module Europeana
   module Mixins
     module ImageVersion
       VERSIONS = {
-        original: nil,
-        full: '1600x1600',
-        fullx2: '3200x3200',
-        half: '800x800',
-        halfx2: '1600x1600',
-        small: '400x400',
-        smallx2: '800x800',
-        thumbnail: '400x400',
-        thumbnailx2: '800x800'
+        #original: {size: nil, format: 'jpeg', 'quality': 85 },
+        full: {size: '1600x1600', format: 'jpeg', 'quality': 85 },
+        fullx2: {size: '3200x3200', format: 'jpeg', 'quality': 85 },
+        half: {size: '800x800', format: 'jpeg', 'quality': 85 },
+        halfx2: {size: '1600x1600', format: 'jpeg', 'quality': 85 },
+        small: {size: '400x400', format: 'jpeg', 'quality': 85 },
+        smallx2: {size: '800x800', format: 'jpeg', 'quality': 85 },
+        thumbnail: {size: '400x400', format: 'jpeg', 'quality': 85 },
+        thumbnailx2: {size: '800x800', format: 'jpeg', 'quality': 85 },
+        thumbnail_png: {size: '400x400', format: 'png', 'quality': nil }
       }
 
       def versions(name = 'image')
         image = @element.content_by_name(name)
         return nil if image.nil?
-        Hash[VERSIONS.map {|version,size| [version, 
+        Hash[VERSIONS.map {|version,settings| [version,
           {
-            url: image.essence.picture_url(image_size: size)
+            url: image.essence.picture_url(image_size: settings[:size], format: settings[:format], quality: settings[:quality])
           }
         ]}]
       end
