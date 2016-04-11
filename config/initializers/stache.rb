729cf7510b40052af2357f122db2b7427222aaca
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-04-11 11:59:16 +0200

    Cache mustache templates

---
+++ config/initializers/stache.rb
@@ -0,0 +1,10 @@
+##
+# Configure the stache gem
+#
+# @see https://github.com/agoragames/stache
+Stache.configure do |c|
+  # Store compiled templates in memory
+  # (stache template cache does not work with Redis; see
+  # https://github.com/agoragames/stache/issues/58)
+  c.template_cache = ActiveSupport::Cache::MemoryStore.new if Rails.env.production?
+end
