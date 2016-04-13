a461f0eb89fae2cd72f3e0981cec2123043ba38c
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-04-13 15:21:50 +0200

    Remove some logging statements

--- app/jobs/europeana/picture_versions_job.rb
+++ app/jobs/europeana/picture_versions_job.rb
@@ -2,14 +2,11 @@ class Europeana::PictureVersionsJob < ActiveJob::Base
   queue_as :default
 
   def perform(id)
-    Rails.logger.info "Create versions for picture with id #{id}"
-
     Europeana::Elements::Image::VERSIONS.values.compact.each do |settings|
       Rails.logger.info "Creating #{JSON.generate(settings)} of picture #{id}"
       next if settings[:size].nil?
 
       picture = Alchemy::Picture.find(id)
-
       options = []
 
       if settings[:format] && settings[:format] == 'jpeg'  && settings[:quality]
@@ -21,10 +18,8 @@ class Europeana::PictureVersionsJob < ActiveJob::Base
       end
 
       if settings[:size] && settings[:crop]
-        Rails.logger.info "Cropping"
         picture = picture.crop(settings[:size], false, false, settings[:upsample] || false)
       else
-        Rails.logger.info "Resizing"
         picture = picture.resize(settings[:size])
       end
 
