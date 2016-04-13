b2a5d4269fb1788c6b8d27e9c5ea25c1d7709339
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-04-13 15:45:59 +0200

    Update middleware

--- lib/europeana/gsub.rb
+++ lib/europeana/gsub.rb
@@ -3,8 +3,8 @@ require 'rack-plastic'
 module Europeana
   class Gsub < Rack::Plastic
     def change_html_string(html)
-      html = html.gsub(/("|\'|&quot;)\/pictures\/|(^\/pictures\/)/) do | match |
-        "#{$1}//#{ENV.fetch('CDN_HOST')}/pictures/"
+      html = html.gsub(/("|\'|&quot;)\/portal\/exhibitions\/pictures\/|(^\/portal\/exhibitions\/pictures\/)/) do | match |
+        "#{$1}//#{ENV.fetch('CDN_HOST')}/portal/exhibitions/pictures/"
       end
       html
     end
