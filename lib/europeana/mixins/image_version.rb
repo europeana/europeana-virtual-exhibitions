56431b7ba7a284aec94bc7d065ec5436beee5111
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-04-13 14:22:32 +0200

    Fix cropping

--- lib/europeana/mixins/image_version.rb
+++ lib/europeana/mixins/image_version.rb
@@ -1,6 +1,16 @@
 module Europeana
   module Mixins
     module ImageVersion
+      # Hash that defines which versions of an image are
+      # available and will be created on upload
+
+      # The key is the name of the version and the value is
+      # a simple hash with the following keys:
+      # size: nil|100x100
+      # format: jpeg|png
+      # quality: nil|85 (jpeg quality)
+      # crop: true if the image can be cropped
+      # upsample: true if you want the image to use the entire canvas
       VERSIONS = {
         original: {size: nil, format: 'jpeg', 'quality': nil },
         full: {size: '1600x1600', format: 'jpeg', 'quality': 85 },
@@ -20,7 +30,7 @@ module Europeana
         image = @element.content_by_name(name)
         return false if image.nil? || image.essence.picture.nil?
         Hash[VERSIONS.map {|version,settings|
-          options = { image_size: settings[:size], format: settings[:format], quality: settings[:quality] }
+          options = { image_size: settings[:size], format: settings[:format] }
           if settings[:crop]
             options.merge!({crop_size: 1, crop: 1, crop_from: 1, upsample: settings[:upsample] || false})
           end
