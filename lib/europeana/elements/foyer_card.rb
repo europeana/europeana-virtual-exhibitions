4574a11ddd48b65eda44c06183f7d5adf3814793
Richard Doe <richard.doe@rwdit.net>
2016-04-19 10:38:02 +0100

    Undo bad change

--- lib/europeana/elements/foyer_card.rb
+++ lib/europeana/elements/foyer_card.rb
@@ -8,7 +8,6 @@ module Europeana
           state_1_label: get(:state_1_label, :body),
           state_1_title: get(:state_1_title, :body),
           state_1_image: versions('state_1_image'),
-          state_1_link: url: get(:url, :link),
           state_2_title: get(:state_2_title, :body),
           state_2_body: get(:state_2_body, :body),
           state_2_image: get(:state_2_image, :picture) ? versions('state_2_image') : versions('state_1_image'),
