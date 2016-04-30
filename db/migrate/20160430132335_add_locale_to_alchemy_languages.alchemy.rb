0fa2055ab004a641b4eb8bdb4c14deec768d36e2
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-04-30 15:42:04 +0200

    Revert "Messed up migrations"
    This reverts commit 6da12ca9753e221a03b34eeca1ac45ea1f295dee.

--- db/migrate/20160430132335_add_locale_to_alchemy_languages.alchemy.rb
+++ db/migrate/20160430132335_add_locale_to_alchemy_languages.alchemy.rb
@@ -1,7 +1,7 @@
 # This migration comes from alchemy (originally 20150906195818)
 class AddLocaleToAlchemyLanguages < ActiveRecord::Migration
   def change
-    #add_column :alchemy_languages, :locale, :string
+    add_column :alchemy_languages, :locale, :string
     execute \
       "UPDATE #{Alchemy::Language.table_name} SET locale = language_code WHERE locale IS NULL;"
   end
