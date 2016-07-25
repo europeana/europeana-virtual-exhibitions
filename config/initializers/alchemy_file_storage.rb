de7026279934afbe921c2ad80aae2ec584083b6c
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-07-25 17:37:37 +0200

    assign default values for AWS settings

--- config/initializers/alchemy_file_storage.rb
+++ config/initializers/alchemy_file_storage.rb
@@ -15,10 +15,10 @@ openstack_defaults = {
 }
 
 s3_defaults = {
-  bucket_name: ENV.fetch('AWS_DIRECTORY'),
-  access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-  secret_access_key: ENV.fetch('AWS_SECRET_ACCES_KEY'),
-  region: ENV.fetch('AWS_REGION')
+  bucket_name: ENV.fetch('AWS_DIRECTORY', ''),
+  access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', ''),
+  secret_access_key: ENV.fetch('AWS_SECRET_ACCES_KEY', ''),
+  region: ENV.fetch('AWS_REGION', '')
 }
 
 filestorage_provider = ENV.fetch('FILE_PROVIDER', 'file')
