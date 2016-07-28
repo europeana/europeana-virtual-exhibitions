04ac0ad69f92af9764d9928e8e877a3709bfd61e
Richard Doe <richard.doe@rwdit.net>
2016-07-28 09:39:24 +0100

    Fix quotes

--- spec/features/locale_session_spec.rb
+++ spec/features/locale_session_spec.rb
@@ -1,7 +1,7 @@
 RSpec.feature 'Locale stored in session' do
   describe 'session' do
     it 'stores the locale' do
-      fr_language = create(:alchemy_language, name: 'French', code: "fr")
+      fr_language = create(:alchemy_language, name: 'French', code: 'fr')
       create(:alchemy_page, public: true, visible: true, name: 'sample', page_layout: 'exhibition_theme_page', language: fr_language)
       visit '/fr/exhibitions/sample'
       visit '/exhibitions/sample'
