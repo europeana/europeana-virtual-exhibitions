22b20d46ea786a38e7dbb90060ed1f604bd5133a
Lutz Biedinger <lutz.biedinger@gmail.com>
2017-06-20 12:24:06 +0200

    syntax styling

--- app/controllers/concerns/alchemy/page_redirects_extension.rb
+++ app/controllers/concerns/alchemy/page_redirects_extension.rb
@@ -6,9 +6,9 @@ module Alchemy
     #
     # == Lookup:
     #
-    # 0. THIS LOGIC IS A EUROPEANA SPECIFC EXTENSION
-    #    If the page is an unpublished 'foyer' we redirect to the foyer of the default language
-    #    + If the default language foyer isn't published we redirect to the first published foyer of any language.
+    # 0. THIS LOGIC IS A EUROPEANA SPECIFIC EXTENSION
+    #    If the page is an unpublished 'foyer'/language_root we redirect to the language_root of the default language
+    #    + If the default language 'foyer' isn't published we redirect to any published language_root of any language.
     #
     # 1. If the page is not published and we have a published child,
     #    we return the url top that page. (Configurable through +redirect_to_public_child+).
@@ -24,41 +24,40 @@ module Alchemy
     def redirect_url
       # This method is being overridden to ensure alternative languages take precedence
       # when trying to find a redirect for unpublished pages
-      @_redirect_url ||= redirect_to_alternative_foyer_url || public_child_redirect_url || controller_and_action_url ||
-                         locale_prefixed_url || nil
+      @_redirect_url ||= redirect_to_alternative_language_root_url || public_child_redirect_url ||
+                         controller_and_action_url || locale_prefixed_url || nil
     end
 
     private
 
-    def redirect_to_alternative_foyer_url
-      if !@page.public? && @page.language_root?
-        page_to_redirect_to = default_language_public_foyer || any_public_foyer
-        if page_to_redirect_to
-          options = {
-            locale: prefix_locale? ? page_to_redirect_to.language_code : nil,
-            urlname: page_to_redirect_to.urlname
-          }
-          alchemy.show_page_path additional_params.merge(options)
-        end
-      end
+    def redirect_to_alternative_language_root_url
+      return if @page.public? || !@page.language_root?
+
+      page_to_redirect_to = default_language_public_language_root || any_public_language_root
+
+      return unless page_to_redirect_to
+
+      options = {
+        locale: prefix_locale? ? page_to_redirect_to.language_code : nil,
+        urlname: page_to_redirect_to.urlname
+      }
+      alchemy.show_page_path additional_params.merge(options)
     end
 
-    # Returns the default language foyer page, if it's published
+    # Returns the default language language_root page, if it's published
     #
     # Otherwise it returns nil.
     #
-    def default_language_public_foyer
-      foyer_page = Language.default.pages.find_by(language_root: true)
-      foyer_page if foyer_page && foyer_page.public?
+    def default_language_public_language_root
+      Language.default.pages.find_by(language_root: true, public: true)
     end
 
-    # Returns the foyer page for the first language it will find, where the foyer page is public.
+    # Returns the language_root page for the first language it will find, where the foyer page is public.
     #
     # Otherwise it returns nil.
     #
-    def any_public_foyer
-      foyer_page = Page.where(language_root: true, public: true).first
-      foyer_page if foyer_page
+    def any_public_language_root
+      Page.find_by(language_root: true, public: true)
     end
   end
 end
