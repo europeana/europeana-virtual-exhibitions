7dc17cf14fdf073fb3c8de0899124ee5d8767217
Lutz Biedinger <lutz.biedinger@gmail.com>
2018-05-02 13:17:50 +0200

    style

--- app/jobs/europeana/picture_versions_cleanup_job.rb
+++ app/jobs/europeana/picture_versions_cleanup_job.rb
@@ -8,7 +8,7 @@ module Europeana
     def perform(id)
       %w(halfx2 small smallx2 thumbnailx2 thumbnail_png).each do |version_key|
         signature = Alchemy::DragonflySignature.find_by(picture_id: id, version_key: version_key)
-        signature.delete if signature # Delete to skip callbacks, orphaned versions should be cleaned up separately.
+        signature&.delete # Delete to skip callbacks, orphaned versions should be cleaned up separately.
       end
     end
   end
