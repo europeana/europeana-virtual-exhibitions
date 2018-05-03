b8f9af2bec2654b97b325b8cbce25c1097b2f396
Lutz Biedinger <lutz.biedinger@gmail.com>
2018-05-03 14:49:08 +0200

    add comment for cleanup jobs

--- app/jobs/europeana/picture_versions_cleanup_job.rb
+++ app/jobs/europeana/picture_versions_cleanup_job.rb
@@ -5,6 +5,9 @@ module Europeana
     queue_as :default
     include PictureVersionHelper
 
+    # These jobs will be ran in order to remove duplicate signatures for differnet version keys.
+    # Once all of these signatures have been removed the job won't be needed anymore and can be removed.
+    #
     def perform(id)
       %w(halfx2 small smallx2 thumbnailx2 thumbnail_png).each do |version_key|
         signature = Alchemy::DragonflySignature.find_by(picture_id: id, version_key: version_key)
