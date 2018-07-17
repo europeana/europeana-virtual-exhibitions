12a9834060533008a5febae5315eb44cebe9756e
Lutz Biedinger <lutz.biedinger@gmail.com>
2018-07-17 14:33:07 +0200

    add missing files/migrations/initializers

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,5 @@
+# AlchemyCMS Dragonfly configuration.
+#
+# By default AlchemyCMS generates a dragonfly initializer here.
+# Fore Europeana Virtual Exhibitions this initialization is in:
+# config/initializers/alchemy_file_storage.rb
