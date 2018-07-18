585f5ff48860521cb7e39914ee2c3a1a67c8345a
Lutz Biedinger <lutz.biedinger@gmail.com>
2018-07-18 15:06:10 +0200

    tidy, use simplecov for travis testcoverage

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,5 +1,5 @@
 # AlchemyCMS Dragonfly configuration.
 #
 # By default AlchemyCMS generates a dragonfly initializer here.
-# Fore Europeana Virtual Exhibitions this initialization is in:
+# For Europeana Virtual Exhibitions this initialization is in:
 # config/initializers/alchemy_file_storage.rb
